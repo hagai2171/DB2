bc614a45da8b41b07cf8e2c8d92cb7624d012cad
yinon2592 <82467001+yinon2592@users.noreply.github.com>
2023-05-31 20:56:18 +0300

    add photo complete

--- code/Solution.py
+++ code/Solution.py
@@ -7,9 +7,35 @@ from Business.RAM import RAM
 from Business.Disk import Disk
 from psycopg2 import sql
 
+# ************************************** our auxiliary functions start **************************************
+# generically add tuple to table
+def add(query) -> ReturnValue:
+    result = ReturnValue.OK
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        conn.execute(query)
+        conn.commit()
+    except (DatabaseException.CHECK_VIOLATION,
+            DatabaseException.NOT_NULL_VIOLATION,
+            DatabaseException.FOREIGN_KEY_VIOLATION) as e:
+        result = ReturnValue.BAD_PARAMS
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        result = ReturnValue.ALREADY_EXISTS
+    except (DatabaseException.ConnectionInvalid,
+            DatabaseException.database_ini_ERROR,
+            DatabaseException.UNKNOWN_ERROR,
+            Exception) as e:
+        result = ReturnValue.ERROR
+    finally:
+        # will happen any way after try termination or exception handling
+        conn.close()
+        return result
+# ************************************** our auxiliary functions end **************************************
 
+# ************************************** Database functions start **************************************
 def createTables():
-    base_tables = """"
+    base_tables = """
         CREATE TABLE IF NOT EXISTS "Photo"
             (
                 id integer NOT NULL PRIMARY KEY CHECK (id > 0),
@@ -21,7 +47,7 @@ def createTables():
                 id integer NOT NULL PRIMARY KEY CHECK (id > 0),
                 manufacturing_company TEXT NOT NULL,
                 speed integer NOT NULL CHECK (speed > 0),
-                free_space integer NOT NULL CHECK (disk_free_space_needed >= 0),
+                free_space integer NOT NULL CHECK (free_space >= 0),
                 cost_per_byte integer NOT NULL CHECK (cost_per_byte > 0)
             );
          CREATE TABLE IF NOT EXISTS "Ram"
@@ -42,21 +68,10 @@ def createTables():
 
     conn = None
     try:
+        # breakpoint()
         conn = Connector.DBConnector()
-        conn.execute(query )
+        conn.execute(query)
         conn.commit()
-    except DatabaseException.ConnectionInvalid as e:
-        print(e)
-    except DatabaseException.NOT_NULL_VIOLATION as e:
-        print(e)
-    except DatabaseException.CHECK_VIOLATION as e:
-        print(e)
-    except DatabaseException.UNIQUE_VIOLATION as e:
-        print(e)
-    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
-        print(e)
-    except Exception as e:
-        print(e)
     finally:
         # will happen any way after try termination or exception handling
         conn.close()
@@ -70,15 +85,45 @@ def clearTables():
 def dropTables():
     pass
 
+# ************************************** Database functions end **************************************
 
+# ************************************** CRUD API functions start **************************************
+# Python's equivalent to NULL is None.
+# You can assume the arguments to the function will not be None, the inner attributes of the
+# argument might consist of None
 def addPhoto(photo: Photo) -> ReturnValue:
-    return ReturnValue.OK
+    query = sql.SQL('INSERT INTO "Photo" VALUES ({photo_id}, {description}, {disk_free_space_needed})').format(
+        photo_id=sql.Literal(photo.getPhotoID()),
+        description=sql.Literal(photo.getDescription()),
+        disk_free_space_needed=sql.Literal(photo.getSize())
+    )
+    return add(query)
 
 
 def getPhotoByID(photoID: int) -> Photo:
-    return Photo()
+    # Photo getPhotoByID(Int photoID)
+    # Returns the photo object of photoID.
+    # Input: Photo ID.
+    # Output: The photo object in case the photo exists. BadPhoto() otherwise
+    result = Photo.badPhoto()
+    query = sql.SQL('SELECT * FROM "Photo" WHERE id = {id} ').format(id=sql.Literal(photoID))
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        row_effected, entries = conn.execute(query)
+        if row_effected == 1:
+            photo_id, description, size = entries[0].values()
+            result = Photo(photo_id, description, size)
+    except Exception as e:
+        pass
+    finally:
+        conn.close()
+        return result
 
 
+# createTables()
+# addPhoto(Photo(1, "Tree", 10))
+# print(getPhotoByID(1).__str__())
 def deletePhoto(photo: Photo) -> ReturnValue:
     return ReturnValue.OK
 
@@ -106,7 +151,9 @@ def getRAMByID(ramID: int) -> RAM:
 def deleteRAM(ramID: int) -> ReturnValue:
     return ReturnValue.OK
 
+# ************************************** CRUD API functions end **************************************
 
+# ************************************** BASIC API functions start **************************************
 def addDiskAndPhoto(disk: Disk, photo: Photo) -> ReturnValue:
     return ReturnValue.OK
 
@@ -158,6 +205,9 @@ def isDiskContainingAtLeastNumExists(description : str, num : int) -> bool:
 def getDisksContainingTheMostData() -> List[int]:
     return []
 
+# ************************************** BASIC API functions end **************************************
+
+# ************************************** ADVANCED API functions start **************************************
 
 def getConflictingDisks() -> List[int]:
     return []
@@ -169,3 +219,5 @@ def mostAvailableDisks() -> List[int]:
 
 def getClosePhotos(photoID: int) -> List[int]:
     return []
+
+# ************************************** ADVANCED API functions end **************************************
