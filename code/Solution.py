eb4473c4d89072d1af4dcbc546a0176da2bfaac4
hagai2171 <hagai.g2171@gmail.com>
2023-06-03 17:29:56 +0300

    more functions

--- code/Solution.py
+++ code/Solution.py
@@ -66,7 +66,15 @@ def create_ram_in_disk_table():
 
 def create_view_tables():
     return """
+        CREATE VIEW "TotalRAMInDisk" as 
+        select "Disk".id as disk_id,COALESCE(SUM("RAM".size), 0) as total_ram
+        from "Disk"
+        left outer join "RAMInDisk" on "Disk".id = "RAMInDisk".disk_id
+        left outer join  "RAM" on "RAM".id = "RAMInDisk".ram_id
+        GROUP BY "Disk".id;
     """
+
+
 # generically add tuple to table
 def add(query) -> ReturnValue:
     result = ReturnValue.OK
@@ -146,7 +154,9 @@ def clearTables():
 def dropTables():
     base_tables = ["Photo", "Disk", "RAM"]
     new_tables = ["PhotoInDisk", "RAMInDisk"]
-    queries = ['DROP TABLE IF EXISTS "{table}" CASCADE;'.format(table=table) for table in base_tables + new_tables]
+    view_tables = ["TotalRAMInDisk"]
+    queries = ['DROP TABLE IF EXISTS "{table}" CASCADE;'.format(table=table) for table in
+               base_tables + new_tables + view_tables]
     query = "\n".join(queries)
     conn = None
     try:
@@ -209,7 +219,8 @@ def deletePhoto(photo: Photo) -> ReturnValue:
 
 
 def addDisk(disk: Disk) -> ReturnValue:
-    query = sql.SQL('INSERT INTO "Disk" VALUES ({disk_id}, {manufacturing_company}, {speed}, {free_space}, {cost_per_byte});').format(
+    query = sql.SQL(
+        'INSERT INTO "Disk" VALUES ({disk_id}, {manufacturing_company}, {speed}, {free_space}, {cost_per_byte});').format(
         disk_id=sql.Literal(disk.getDiskID()),
         manufacturing_company=sql.Literal(disk.getCompany()),
         speed=sql.Literal(disk.getSpeed()),
@@ -244,6 +255,7 @@ def deleteDisk(diskID: int) -> ReturnValue:
     query = sql.SQL('DELETE FROM "Disk" where id = {id}').format(id=sql.Literal(diskID))
     return delete(query=query, is_disk=True)
 
+
 def addRAM(ram: RAM) -> ReturnValue:
     query = sql.SQL('INSERT INTO "RAM" VALUES ({id}, {size}, {company})').format(
         id=sql.Literal(ram.getRamID()),
@@ -276,28 +288,14 @@ def deleteRAM(ramID: int) -> ReturnValue:
     query = sql.SQL(
         'DELETE FROM "RAM" where id = {id}').format(
         id=sql.Literal(ramID))
-    conn = None
-    try:
-        conn = Connector.DBConnector()
-        rows_effected, _ = conn.execute(query)
-        if rows_effected == 0:
-            return ReturnValue.NOT_EXISTS
-        conn.commit()
-    except DatabaseException.ConnectionInvalid as e:
-        return ReturnValue.ERROR
-    except Exception as e:
-        return ReturnValue.ERROR
-    finally:
-        conn.close()
-
-    return ReturnValue.OK
+    return delete(query=query, is_disk=False)
 
 
 def addDiskAndPhoto(disk: Disk, photo: Photo) -> ReturnValue:
     query = sql.SQL("""
-    INSERT INTO "Disk" VALUES ({disk_id}, {manufacturing_company}, {speed}, {free_space}, {cost_per_byte});
-    INSERT INTO "Photo" VALUES ({photo_id}, {description}, {disk_free_space_needed});
-    """).format(
+        INSERT INTO "Disk" VALUES ({disk_id}, {manufacturing_company}, {speed}, {free_space}, {cost_per_byte});
+        INSERT INTO "Photo" VALUES ({photo_id}, {description}, {disk_free_space_needed});
+        """).format(
         disk_id=sql.Literal(disk.getDiskID()),
         manufacturing_company=sql.Literal(disk.getCompany()),
         speed=sql.Literal(disk.getSpeed()),
@@ -308,32 +306,36 @@ def addDiskAndPhoto(disk: Disk, photo: Photo) -> ReturnValue:
         disk_free_space_needed=sql.Literal(photo.getSize())
     )
     return add(query)
+
+
 # ************************************** CRUD API functions end **************************************
 
 # ************************************** BASIC API functions start **************************************
 
 def addPhotoToDisk(photo: Photo, diskID: int) -> ReturnValue:
     query = sql.SQL("""
-    INSERT INTO "PhotoInDisk" values ({photo_id},{disk_id});
-    UPDATE "Disk" set free_space = free_space - {photo_size} where id = {disk_id};
+        INSERT INTO "PhotoInDisk" values ({photo_id},{disk_id});
+        UPDATE "Disk" set free_space = free_space - {photo_size} where id = {disk_id};
     """).format(
         photo_id=sql.Literal(photo.getPhotoID()),
         photo_size=sql.Literal(photo.getSize()),
         disk_id=sql.Literal(diskID))
     return add(query)
 
-# clearTables()
-# createTables()
-# print( addDiskAndPhoto(Disk(22, "DELL", 10, 20, 10), Photo(1, "Tree", 10)) )
-# addPhoto(Photo(1, "Tree", 10))
-# addDisk(Disk(22, "DELL", 10, 20, 10))
-# addPhotoToDisk(Photo(1, "Tree", 10), 22)
-# breakpoint()
-# deleteDisk(22)
-# clearTables()
 
 def removePhotoFromDisk(photo: Photo, diskID: int) -> ReturnValue:
-    return ReturnValue.OK
+    query = sql.SQL("""
+        UPDATE "Disk" set free_space=free_space+(
+            select "Photo".disk_free_space_needed
+            from "Photo"
+            inner join  "PhotoInDisk" on "PhotoInDisk".disk_id = {diskID} and "Photo".id = "PhotoInDisk".photo_id and "Photo".id= {photoID}
+            ) where id = {diskID};
+        DELETE FROM "PhotoInDisk" where Photo_id = {photoID} and disk_id = {diskID};
+        """).format(
+        photoID=sql.Literal(photo.getPhotoID()),
+        PhotoSize=sql.Literal(photo.getSize()),
+        diskID=sql.Literal(diskID))
+    return delete(query=query, is_disk=False)
 
 
 def addRAMToDisk(ramID: int, diskID: int) -> ReturnValue:
@@ -344,37 +346,103 @@ def addRAMToDisk(ramID: int, diskID: int) -> ReturnValue:
     return add(query)
 
 
-# dropTables()
-clearTables()
-# createTables()
-addRAM(RAM(1, "a", 2))
-addRAM(RAM(2, "a", 5))
-# addRAM(RAM(3, "a", 5))
-addRAMToDisk(1, 1)
-
-
-# print(getRAMByID(1))
-# deleteRAM(1)
-# deleteRAM(2)
-# print(getRAMByID(1))
-# clearTables()
-# dropTables()
 def removeRAMFromDisk(ramID: int, diskID: int) -> ReturnValue:
-    return ReturnValue.OK
+    query = sql.SQL("""
+        DELETE FROM "RAMInDisk" where ram_id = {ramID} and disk_id = {diskID};
+        """).format(
+        ramID=sql.Literal(ramID),
+        diskID=sql.Literal(diskID))
+    return delete(query=query, is_disk=False)
 
 
 def averagePhotosSizeOnDisk(diskID: int) -> float:
-    return 0
+    avg_size = 0
+    query = sql.SQL("""
+           select AVG("Photo".disk_free_space_needed)
+           from "Photo"
+           inner join "PhotoInDisk" on "PhotoInDisk".disk_id = {diskID} and "Photo".id = "PhotoInDisk".photo_id
+           """).format(
+        diskID=sql.Literal(diskID))
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        row_effected, entries = conn.execute(query)
+        if row_effected != 0:
+            avg_size = entries.rows[0][0]
+    except DatabaseException.ConnectionInvalid as e:
+        return -1
+    except Exception as e:
+        print(e)
+        return -1
+    finally:
+        conn.close()
+    return avg_size
 
 
 def getTotalRamOnDisk(diskID: int) -> int:
-    return 0
+    total_ram_available = 0
+    query = sql.SQL("""
+        select total_ram
+        from "TotalRAMInDisk"
+        where "TotalRAMInDisk".disk_id = {diskID}
+        """).format(
+        diskID=sql.Literal(diskID))
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        row_effected, entries = conn.execute(query)
+        if row_effected != 0:
+            total_ram_available = entries.rows[0][0]
+    except DatabaseException.ConnectionInvalid as e:
+        return -1
+    except Exception as e:
+        print(e)
+        return -1
+    finally:
+        conn.close()
+    return total_ram_available
 
 
 def getCostForDescription(description: str) -> int:
-    return 0
+    cost = 0
+    query = sql.SQL("""
+        select sum("Disk".cost_per_byte * "Photo".disk_free_space_needed)
+        from "Disk"
+        inner join "PhotoInDisk" on "PhotoInDisk".disk_id = "Disk".id
+        inner join "Photo" on "Photo".id = "PhotoInDisk".photo_id and "Photo".description = {description}
+        """).format(
+        description=sql.Literal(description))
 
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        row_effected, entries = conn.execute(query)
+        if row_effected != 0:
+            cost = entries.rows[0][0]
+    except DatabaseException.ConnectionInvalid as e:
+        return -1
+    except Exception as e:
+        print(e)
+        return -1
+    finally:
+        conn.close()
+    return cost
 
+# clearTables()
+# createTables()
+# addDisk(Disk(1,"IBM", 100, 100000,1))
+# addPhoto(Photo(1, "IBM", 100))
+# addPhoto(Photo(2, "hagai", 100))
+# addPhoto(Photo(5, "hagai", 55))
+# addPhoto(Photo(7, "hagai", 3))
+# addPhoto(Photo(3, "IBM", 100))
+#
+# addPhotoToDisk(Photo(1, "IBM", 100), 1)
+# addPhotoToDisk(Photo(2, "hagai", 100), 1)
+# addPhotoToDisk(Photo(5, "hagai", 55), 1)
+# addPhotoToDisk(Photo(7, "hagai", 3), 1)
+# addPhotoToDisk(Photo(3, "IBM", 100), 1)
+# print(getCostForDescription("hagai"))
 def getPhotosCanBeAddedToDisk(diskID: int) -> List[int]:
     return []
 
@@ -387,7 +455,7 @@ def isCompanyExclusive(diskID: int) -> bool:
     return True
 
 
-def isDiskContainingAtLeastNumExists(description : str, num : int) -> bool:
+def isDiskContainingAtLeastNumExists(description: str, num: int) -> bool:
     return True
 
 
